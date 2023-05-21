4f6b71ed96fe2e56d0066a6cbdebf33b6f7d3f28
imdancho <daniyar.0586@gmail.com>
2023-05-21 22:04:00 +0600

    ads_ass_5

---
+++ src/BST.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+
+public class BST <K extends Comparable<K>, V> {
+    private Node root;
+    private int size;
+
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+}

df508c4e30d5eab9e8edeaa3eae3c0a8c588f70f
imdancho <daniyar.0586@gmail.com>
2023-05-22 00:20:50 +0600

    ads_ass_5

--- src/BST.java
+++ src/BST.java
@@ -1,8 +1,8 @@
-import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
 
-public class BST <K extends Comparable<K>, V> {
+public class BST<K extends Comparable<K>, V> {
     private Node root;
-    private int size;
 
     private class Node {
         private K key;
@@ -13,5 +13,119 @@ public class BST <K extends Comparable<K>, V> {
             this.key = key;
             this.val = val;
         }
+    }
+
+    private Node put(Node node, K key, V val) {
+
+        if (node == null) return new Node(key, val);
+
+        int cccc = key.compareTo( node.key );
+
+        if (cccc < 0) node.left = put(node.left, key, val);
+        else if (cccc > 0) node.right = put(node.right, key, val);
+        else node.val = val;
+        return node;
+
+    }
+
+    public void put(K key, V value) {
+
+        root = put(root, key, value);
+
+    }
+
+    private Node get(Node node, K key) {
+
+        if (node == null) return null;
+
+        int cmp = key.compareTo(node.key);
+
+        if (cmp < 0) return get(node.left, key);
+        else if (cmp > 0) return get(node.right, key);
+        else return node;
+
+    }
+
+    public V get(K key) {
+
+        Node nod = get(root, key);
+        return nod != null ? nod.val : null;
+
+    }
+
+    public void delete(K key) {
+
+        root = delete(root, key);
+
+    }
+
+    private Node delete(Node node, K key) {
+        if (node == null)   return null;
+
+        int cmp = key.compareTo(node.key);
+
+        if (cmp < 0) {
+            node.left = delete(node.left, key);
+        }
+        else if (cmp > 0) {
+            node.right = delete(node.right, key);
+        }
+        else {
+
+            if (node.left == null) {
+                return node.right;
+            }
+            else if (node.right == null) {
+                return node.left;
+            }
+            else {
+                Node minRightNode = findMin(node.right);
+                node.key = minRightNode.key;
+                node.val = minRightNode.val;
+                node.right = delete(node.right, minRightNode.key);
+            }
+
+        }
+        return node;
+    }
+
+    public Node findMin(Node node) {
+
+        while (node.left != null) {
+            node = node.left; }
+        return node;
+
+    }
+
+    public Iterable<K> iterator() {
+
+        List<K> keysss = new ArrayList<>();
+        inorderTraversal(root, keysss);
+        return keysss;
+
+    }
+
+    private void inorderTraversal(Node node, List<K> keys) {
+
+        if (node == null) return;
+
+        inorderTraversal(node.left, keys);
+
+        keys.add(node.key);
+        inorderTraversal(node.right, keys);
+
+    }
+
+    public int size() {
+
+        return size(root);
+
+    }
+
+    private int size(Node node) {
+
+        if (node == null) return 0;
+        else return 1 + size(node.left) + size(node.right);
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BST<Integer, String> bst = new BST<>();
+
+        bst.put(1, "1");
+        bst.put(2, "2");
+        bst.put(3, "3");
+        bst.put(4, "4");
+        bst.put(5, "5");
+        bst.put(6, "6");
+
+        System.out.println("4: " + bst.get(4));
+
+
     }
 }

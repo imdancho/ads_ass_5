9283abe23485f4468caeda627c3b793e93648881
imdancho <daniyar.0586@gmail.com>
2023-05-22 09:33:47 +0600

    ads_ass_5

--- src/BST.java
+++ src/BST.java
@@ -128,4 +128,30 @@ public class BST<K extends Comparable<K>, V> {
         else return 1 + size(node.left) + size(node.right);
 
     }
+
+    public int height() {
+
+        return height(root);
+
+    }
+
+    private int height(Node node) {
+
+        if (node == null) {
+            return 0;
+        }
+        else {
+            int lHeight = height(node.left);
+            int rHeight = height(node.right);
+
+            if (lHeight > rHeight) {
+                return lHeight + 1;
+            }
+            else {
+                return rHeight + 1;
+            }
+
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,30 @@ public class Main {
     public static void main(String[] args) {
         BST<Integer, String> bst = new BST<>();
 
-        bst.put(1, "1");
-        bst.put(2, "2");
-        bst.put(3, "3");
-        bst.put(4, "4");
-        bst.put(5, "5");
-        bst.put(6, "6");
+//        bst.put(1, "1");
+//        bst.put(2, "2");
+//        bst.put(3, "3");
+//        bst.put(4, "4");
+//        bst.put(5, "5");
+//        bst.put(6, "6");
+//        bst.put(0, "0");
+//        bst.put(-1, "-1");
+//        bst.put(-2, "-2");
 
-        System.out.println("4: " + bst.get(4));
+        bst.put(15, "15");
+        bst.put(32, "32");
+        bst.put(65, "65");
+        bst.put(13, "13");
+        bst.put(0, "0");
+        bst.put(5, "5");
+        bst.put(10, "10");
+        bst.put(31, "31");
+        bst.put(9, "9");
+        bst.put(3, "3");
+
+
+        System.out.println("Height: " + (bst.height()-1));
+
 
 
     }
